7b4059b7498c35ecbfd549c0610dfe87acfe2afc
RezoGraf <ctsaverno@gmail.com>
2021-11-24 19:36:29 +0700

    ав

--- app.py
+++ app.py
@@ -93,6 +93,9 @@ tv2str1n1 = """select np_otd.notd,n_mpp.nmpp,
     order by PS"""
 
 
+
+
+
 tv2str2n1 = """select np_otd.notd,n_mpp.nmpp,
   (select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),
   (select nroom_kr as rname from room where room.id=it_rasp.room),
@@ -113,6 +116,28 @@ tv2str2n1 = """select np_otd.notd,n_mpp.nmpp,
     where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
     and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=15
     and (it_rasp.ntv=2) and (it_rasp.nlist=2) order by PS"""
+
+tv2str2n2 = """select np_otd.notd,n_mpp.nmpp,
+  (select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),
+  (select nroom_kr as rname from room where room.id=it_rasp.room),
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.even_day
+  END as even_day,
+
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.noeven_day
+  END as noeven_day,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=6) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as saturday,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=7) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as sunday
+    from it_rasp,np_otd,n_doc,n_mpp
+    where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
+    and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=13
+    and (it_rasp.ntv=2) and (it_rasp.nlist=1)
+    order by PS"""
 
 #
 # tv2str2n2 = """select np_otd.notd,n_mpp.nmpp,
@@ -233,27 +258,27 @@ def tv12():
     return render_template('tv12.html', my_list=result1, my_list2=result2, style_evenday_var=style_evenday_var, style_noevenday_var=style_noevenday_var, style_saturday_var=style_saturday_var, style_sunday_var=style_sunday_var)
 
 
-@app.route('/tv21', methods=['GET'])
-def tv21():
-    result = db_select(tv2str1n1)
-    style_evenday_var = style_evenday()
-    style_noevenday_var = style_noevenday()
-    style_saturday_var = style_saturday()
-    style_sunday_var = style_sunday()
-
-    return render_template('tv21.html', my_list=result, style_evenday_var=style_evenday_var, style_noevenday_var=style_noevenday_var, style_saturday_var=style_saturday_var, style_sunday_var=style_sunday_var)
+# @app.route('/tv21', methods=['GET'])
+# def tv21():
+#     result = db_select(tv2str1n1)
+#     style_evenday_var = style_evenday()
+#     style_noevenday_var = style_noevenday()
+#     style_saturday_var = style_saturday()
+#     style_sunday_var = style_sunday()
+#
+#     return render_template('tv21.html', my_list=result, style_evenday_var=style_evenday_var, style_noevenday_var=style_noevenday_var, style_saturday_var=style_saturday_var, style_sunday_var=style_sunday_var)
 
 
 @app.route('/tv22', methods=['GET'])
 def tv22():
     result1 = db_select(tv2str2n1)
-    # result2 = db_select(tv2str2n2)
+    result2 = db_select(tv2str2n2)
     style_evenday_var = style_evenday()
     style_noevenday_var = style_noevenday()
     style_saturday_var = style_saturday()
     style_sunday_var = style_sunday()
 
-    return render_template('tv22.html', my_list1=result1, style_evenday_var=style_evenday_var, style_noevenday_var=style_noevenday_var, style_saturday_var=style_saturday_var, style_sunday_var=style_sunday_var)
+    return render_template('tv22.html', my_list1=result1, my_list2=result2, style_evenday_var=style_evenday_var, style_noevenday_var=style_noevenday_var, style_saturday_var=style_saturday_var, style_sunday_var=style_sunday_var)
 
 
 @app.route('/nohead', methods=['GET'])
