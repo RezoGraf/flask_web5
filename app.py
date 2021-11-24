df0b2b5d51499ed82aa1db2320d8064d88c7d3c4
RezoGraf <ctsaverno@gmail.com>
2021-11-24 19:36:29 +0700

    ав

--- app.py
+++ app.py
@@ -1,46 +1,115 @@
 import fdb
-from flask import Flask, abort, request, render_template, json, render_template_string
+from flask import Flask, abort, request, render_template, json, render_template_string, url_for
 
 app = Flask(__name__)
-# Соединение
-# con = fdb.connect(dsn='192.168.100.9:C:/DB/ARENA.GDB', user='sysdba', password='masterkey')
-# Объект курсора
-# cur = con.cursor()
-# # Выполняем запрос
-# try:
-#     # Выборка данных
-#     cur.execute("""select np_otd.notd,n_mpp.nmpp,(select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),(select nroom_kr as rname from room where room.id=it_rasp.room),it_rasp.even_day,it_rasp.noeven_day,saturday,sunday
-# from it_rasp,np_otd,n_doc,n_mpp
-# where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc) and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=12
-# and (it_rasp.ntv=1)
-# order by it_rasp.lpu,notd,nmpp""")
-#     # Обработка строк выборки
-#     result = cur.fetchall()
-#     for x in result:
-#         print(x);
-# except:
-#     con.rollback()
-tv1 = """select np_otd.notd,n_mpp.nmpp,(select ndlj from n_dlj
-    where n_doc.dolj=n_dlj.dlj),(select nroom_kr as rname from room
-    where room.id=it_rasp.room),it_rasp.even_day,it_rasp.noeven_day,saturday,sunday
+
+tv1str1n1 = """select np_otd.notd,n_mpp.nmpp,
+  (select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),
+  (select nroom_kr as rname from room where room.id=it_rasp.room),
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.even_day
+  END as even_day,
+
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.noeven_day
+  END as noeven_day,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=6) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as saturday,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=7) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as sunday
     from it_rasp,np_otd,n_doc,n_mpp
     where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
     and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=12
     and (it_rasp.ntv=1)
     order by it_rasp.lpu,notd,nmpp"""
 
-tv2 = """select np_otd.notd,n_mpp.nmpp,(select ndlj from n_dlj
-    where n_doc.dolj=n_dlj.dlj),(select nroom_kr as rname from room
-    where room.id=it_rasp.room),it_rasp.even_day,it_rasp.noeven_day,saturday,sunday
+tv1str2n1 = """select np_otd.notd,n_mpp.nmpp,
+  (select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),
+  (select nroom_kr as rname from room where room.id=it_rasp.room),
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.even_day
+  END as even_day,
+
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.noeven_day
+  END as noeven_day,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=6) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as saturday,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=7) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as sunday
     from it_rasp,np_otd,n_doc,n_mpp
     where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
     and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=16
+    and (it_rasp.ntv=1)
+    order by it_rasp.lpu,notd,nmpp"""
+
+
+tv1str2n2 = """select np_otd.notd,n_mpp.nmpp,
+  (select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),
+  (select nroom_kr as rname from room where room.id=it_rasp.room),
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.even_day
+  END as even_day,
+
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.noeven_day
+  END as noeven_day,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=6) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as saturday,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=7) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as sunday
+    from it_rasp,np_otd,n_doc,n_mpp
+    where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
+    and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=14
+    and (it_rasp.ntv=1)
+    order by it_rasp.lpu,notd,nmpp"""
+
+
+tv2str1n1 = """select np_otd.notd,n_mpp.nmpp,
+  (select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),
+  (select nroom_kr as rname from room where room.id=it_rasp.room),
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.even_day
+  END as even_day,
+
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.noeven_day
+  END as noeven_day,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=6) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as saturday,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=7) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as sunday
+    from it_rasp,np_otd,n_doc,n_mpp
+    where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
+    and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=15
     and (it_rasp.ntv=2)
     order by it_rasp.lpu,notd,nmpp"""
 
-tv3 = """select np_otd.notd,n_mpp.nmpp,(select ndlj from n_dlj
-    where n_doc.dolj=n_dlj.dlj),(select nroom_kr as rname from room
-    where room.id=it_rasp.room),it_rasp.even_day,it_rasp.noeven_day,saturday,sunday
+
+tv2str1n2 = """select np_otd.notd,n_mpp.nmpp,
+  (select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),
+  (select nroom_kr as rname from room where room.id=it_rasp.room),
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.even_day
+  END as even_day,
+
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.noeven_day
+  END as noeven_day,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=6) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as saturday,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=7) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as sunday
     from it_rasp,np_otd,n_doc,n_mpp
     where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
     and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=14
@@ -48,46 +117,81 @@ tv3 = """select np_otd.notd,n_mpp.nmpp,(select ndlj from n_dlj
     order by it_rasp.lpu,notd,nmpp"""
 
 
-def db_select(ntv) -> object:
-    con4 = fdb.connect(dsn='192.168.100.9:C:/DB/ARENA.GDB', user='sysdba', password='masterkey')
-    cur4 = con4.cursor()
-    cur4.execute(ntv)
-    result4 = cur4.fetchall()
-    cur4.close()
-    return result4
+tv2str2n1 = """select np_otd.notd,n_mpp.nmpp,
+  (select ndlj from n_dlj where n_doc.dolj=n_dlj.dlj),
+  (select nroom_kr as rname from room where room.id=it_rasp.room),
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.even_day
+  END as even_day,
+
+  CASE 
+     when it_rasp.dateoff>=Cast('NOW' as Date) then NULL 
+  ELSE 
+     it_rasp.noeven_day
+  END as noeven_day,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=6) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as saturday,
+(select TIME_DUTY from it_rasp_duty where (it_rasp_duty.doc=it_rasp.doc) and (it_rasp_duty.NDAY=7) and EXTRACT(WEEK from DATE_DUTY)=EXTRACT(WEEK from Cast('NOW' as Date))) as sunday
+    from it_rasp,np_otd,n_doc,n_mpp
+    where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
+    and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and it_rasp.lpu=165 and n_doc.otd=12
+    and (it_rasp.ntv=1)
+    order by it_rasp.lpu,notd,nmpp"""
 
 
-result = db_select(tv1)
-result2 = db_select(tv2)
-result3 = db_select(tv3)
-
-
-# con = fdb.connect(dsn='192.168.100.9:C:/DB/ARENA.GDB', user='sysdba', password='masterkey')
-# cur = con.cursor()
-# cur.execute(tv1)
-# result = cur.fetchall()
-# cur.execute(tv2)
-# result2 = cur.fetchall()
-# cur.close()
-#
-# result = db_select(tv1)
-# del cur
+def db_select(sql):
+    con = fdb.connect(dsn='192.168.100.9:C:/DB/ARENA.GDB', user='sysdba', password='masterkey', charset="utf-8")
+    cur = con.cursor()
+    cur.execute(sql)
+    result = cur.fetchall()
+    cur.close()
+    del cur
+    return result
 
 
 @app.route('/', methods=['GET'])
-def hello():  # put application's code here
+def home():
+
     return render_template('index.html')
 
 
-@app.route('/tv1', methods=['GET'])
-def tv1():
-    return render_template('tv1.html', my_list=result)
+@app.route('/tv11', methods=['GET'])
+def tv11():
+    result_tv1str1n1 = db_select(tv1str1n1)
+
+    return render_template('tv11.html', my_list=result_tv1str1n1)
 
 
-@app.route('/tv2', methods=['GET'])
-def tv2():
-    return render_template('tv2.html', my_list=result2, my_list2=result3)
+@app.route('/tv12', methods=['GET'])
+def tv12():
+    result_tv1str2n1 = db_select(tv1str2n1)
+    result_tv1str2n2 = db_select(tv1str2n2)
+
+    return render_template('tv12.html', my_list=result_tv1str2n1, my_list2=result_tv1str2n2)
+
+
+@app.route('/tv21', methods=['GET'])
+def tv21():
+    result_tv2str1n1 = db_select(tv2str1n1)
+    result_tv2str1n2 = db_select(tv2str1n2)
+
+    return render_template('tv21.html', mylist=result_tv2str1n1, my_list2=result_tv2str1n2)
+
+
+@app.route('/tv22', methods=['GET'])
+def tv22():
+    result_tv2str2n1 = db_select(tv2str2n1)
+    print(result_tv2str2n1)
+
+    return render_template('tv22.html', mylist=result_tv2str2n1)
+
+
+@app.route('/nohead', methods=['GET'])
+def nohead():
+
+    return render_template('nohead.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port="3000")
